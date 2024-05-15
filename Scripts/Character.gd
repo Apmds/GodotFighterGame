e0c4c65993fcb86e0b0e8c2ba1383e1cdeee99fa
Apmds <ap1mrfj@gmail.com>
2024-05-15 16:39:42 +0100

    Added crouch weak and strong attacks

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -141,9 +141,11 @@ var current_frame : float = 0
 
 # Attacks
 var weak_attack : Attack = Attack.new()
-var strong_attack : Attack = Attack.new()
 var weak_air_attack : Attack = Attack.new()
+var weak_crouch_attack : Attack = Attack.new()
+var strong_attack : Attack = Attack.new()
 var strong_air_attack : Attack = Attack.new()
+var strong_crouch_attack : Attack = Attack.new()
 var special_attack : Attack = Attack.new()
 var current_attack : Attack
 var missed_attack = false
@@ -426,6 +428,70 @@ func set_attacks() -> void:
 	strong_air_h1.position_y = 5
 	strong_air_attack.add_hitbox(strong_air_h1)
 
+
+	weak_crouch_attack.set_sprite("CrouchWeakAttack", sprites["CrouchWeakAttack"][1])
+	var weak_crouch_w1 = AttackWindow.new()
+	weak_crouch_w1.set_param(AttackWindow.WindowParams.LENGTH, 3)
+	weak_crouch_w1.set_param(AttackWindow.WindowParams.START_FRAME, 0)
+	weak_crouch_w1.set_param(AttackWindow.WindowParams.NUM_FRAMES, 1)
+
+	var weak_crouch_w2 = AttackWindow.new()
+	weak_crouch_w2.set_param(AttackWindow.WindowParams.LENGTH, 6)
+	weak_crouch_w2.set_param(AttackWindow.WindowParams.START_FRAME, 1)
+	weak_crouch_w2.set_param(AttackWindow.WindowParams.NUM_FRAMES, 2)
+
+	var weak_crouch_w3 = AttackWindow.new()
+	weak_crouch_w3.set_param(AttackWindow.WindowParams.LENGTH, 10)
+	weak_crouch_w3.set_param(AttackWindow.WindowParams.START_FRAME, 3)
+	weak_crouch_w3.set_param(AttackWindow.WindowParams.NUM_FRAMES, 1)
+
+	weak_crouch_attack.add_window(weak_crouch_w1)
+	weak_crouch_attack.add_window(weak_crouch_w2)
+	weak_crouch_attack.add_window(weak_crouch_w3)
+
+	var weak_crouch_h1 = Hitbox.new(self)
+
+	weak_crouch_h1.length = 6
+	weak_crouch_h1.window_number = 1
+	weak_crouch_h1.width = 24.5
+	weak_crouch_h1.height = 35
+	weak_crouch_h1.position_x = 35
+	weak_crouch_h1.position_y = 3
+	weak_crouch_attack.add_hitbox(weak_crouch_h1)
+
+
+	strong_crouch_attack.set_sprite("CrouchStrongAttack", sprites["CrouchStrongAttack"][1])
+	var strong_crouch_w1 = AttackWindow.new()
+	strong_crouch_w1.set_param(AttackWindow.WindowParams.LENGTH, 6)
+	strong_crouch_w1.set_param(AttackWindow.WindowParams.START_FRAME, 0)
+	strong_crouch_w1.set_param(AttackWindow.WindowParams.NUM_FRAMES, 2)
+
+	var strong_crouch_w2 = AttackWindow.new()
+	strong_crouch_w2.set_param(AttackWindow.WindowParams.LENGTH, 10)
+	strong_crouch_w2.set_param(AttackWindow.WindowParams.START_FRAME, 2)
+	strong_crouch_w2.set_param(AttackWindow.WindowParams.NUM_FRAMES, 3)
+	strong_crouch_w2.set_param(AttackWindow.WindowParams.VELOCITY_X, 200)
+	strong_crouch_w2.set_param(AttackWindow.WindowParams.VELOCITY_Y, -40)
+
+	var strong_crouch_w3 = AttackWindow.new()
+	strong_crouch_w3.set_param(AttackWindow.WindowParams.LENGTH, 10)
+	strong_crouch_w3.set_param(AttackWindow.WindowParams.START_FRAME, 5)
+	strong_crouch_w3.set_param(AttackWindow.WindowParams.NUM_FRAMES, 3)
+	strong_crouch_w3.set_param(AttackWindow.WindowParams.HAS_WHIFFLAG, true)
+
+	strong_crouch_attack.add_window(strong_crouch_w1)
+	strong_crouch_attack.add_window(strong_crouch_w2)
+	strong_crouch_attack.add_window(strong_crouch_w3)
+
+	var strong_crouch_h1 = Hitbox.new(self)
+	strong_crouch_h1.length = 6
+	strong_crouch_h1.window_number = 1
+	strong_crouch_h1.width = 40
+	strong_crouch_h1.height = 20
+	strong_crouch_h1.position_x = 10
+	strong_crouch_h1.position_y = 5
+	strong_crouch_attack.add_hitbox(strong_crouch_h1)
+
 func _ready():
 	# Setting the collision size for the character
 	var shape = RectangleShape2D.new()
@@ -512,17 +578,26 @@ func _physics_process(delta : float) -> void:
 
 			if weak_pressed:
 				if is_on_floor():
-					weak_attack.play(self)
+					if Input.is_action_pressed(get_action("crouch")):
+						weak_crouch_attack.play(self)
+					else:
+						weak_attack.play(self)
 				else:
 					weak_air_attack.play(self)
 			elif strong_pressed:
 				if is_on_floor():
-					strong_attack.play(self)
+					if Input.is_action_pressed(get_action("crouch")):
+						strong_crouch_attack.play(self)
+					else:
+						strong_attack.play(self)
 				else:
 					strong_air_attack.play(self)
 			elif special_pressed:
 				if is_on_floor():
-					special_attack.play(self)
+					if Input.is_action_pressed(get_action("crouch")):
+						set_state(State.IDLE)
+					else:
+						special_attack.play(self)
 				else:
 					set_state(State.IDLE)
 	elif state == State.PARRY:
